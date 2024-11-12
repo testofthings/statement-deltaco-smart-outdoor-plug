622d0978a04f9e3e26a5bfa1af2b4968667df46b
Zabrakk <ak4.jarvenpaa@gmail.com>
2024-11-12 12:44:51 +0200

    Updated statement: -Modified TLS port for mX.tuyaey.com backends -Removed unused import

--- smart-outdoor-plug/statement.py
+++ smart-outdoor-plug/statement.py
@@ -2,7 +2,7 @@
 # pylint: disable=pointless-statement
 # pylint: disable=expression-not-assigned
 
-from tdsaf.main import Builder, TLS, DNS, UDP, ARP, EAPOL, ICMP, TCP, DHCP
+from tdsaf.main import Builder, TLS, DNS, UDP, ARP, EAPOL, ICMP, TCP
 
 # Start modeling the IoT system
 system = Builder.new("Deltaco Smart Outdoor Plug")
@@ -28,9 +28,9 @@ udp_broadcast_4 = system.broadcast(UDP(port=30012)) # All zeros
 
 # Defining relevant backend services
 tuya_1 = system.backend("Tuya Smart 1").serve(TLS(auth=True)).dns("a1.tuyaeu.com") #18.193.211.120
-tuya_2 = system.backend("Tuya Smart 2").serve(TLS(auth=True)).dns("m1.tuyaeu.com") #18.194.10.142
+tuya_2 = system.backend("Tuya Smart 2").serve(TLS(auth=True, port=8883)).dns("m1.tuyaeu.com") #18.194.10.142
 tuya_3 = system.backend("Tuya Smart 3").serve(TLS(auth=True)).dns("a3.tuyaeu.com") # 3.121.131.36
-tuya_4 = system.backend("Tuya Smart 4").serve(TLS(auth=True)).dns("m2.tuyaeu.com") # 3.66.126.37
+tuya_4 = system.backend("Tuya Smart 4").serve(TLS(auth=True, port=8886)).dns("m2.tuyaeu.com") # 3.66.126.37
 tuya_images = system.backend("Tuya Images").serve().dns("images.tuyaeu.com") # 3.164.68.75, djivuyxezwp4s.cloudfront.net
 aws = system.backend("AWS").serve(TLS(auth=True)).dns("euimagesd2h2yqnfpu4gl5.cdn5th.com") # 18.165.122.35, ...
 aws_iot_dns = system.backend("AWS IoT DNS").serve(TLS(auth=True)).dns("h3.iot-dns.com") # 76.223.21.194
@@ -46,7 +46,7 @@ smart_plug >> udp_broadcast_1
 smart_plug >> mobile_app / ARP
 smart_plug >> tencent / TCP(port=443)
 smart_plug >> tuya_3 / TLS(auth=True)
-smart_plug >> tuya_4 / TLS(auth=True)
+smart_plug >> tuya_4 / TLS(auth=True, port=8886)
 smart_plug >> aws_iot_dns / TLS(auth=True)
 # TODO; DEVICE SENDS IPDC TO MOBILE APP !!
 
@@ -57,7 +57,7 @@ mobile_app >> udp_broadcast_4
 mobile_app >> any_host / DNS / ARP
 mobile_app >> smart_plub_tcp_port
 mobile_app >> tuya_1 / TLS(auth=True)
-mobile_app >> tuya_2 / TLS(auth=True)
+mobile_app >> tuya_2 / TLS(auth=True, port=8883)
 mobile_app >> tuya_images / TLS(auth=True)
 mobile_app >> aws / TLS(auth=True)
 
