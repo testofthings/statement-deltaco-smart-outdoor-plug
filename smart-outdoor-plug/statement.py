ad9d3116a80812152b9131ca4dd512858ab598aa
Zabrakk <ak4.jarvenpaa@gmail.com>
2025-01-07 16:38:04 +0200

    Updated based on Shodan results

--- smart-outdoor-plug/statement.py
+++ smart-outdoor-plug/statement.py
@@ -2,7 +2,7 @@
 # pylint: disable=pointless-statement
 # pylint: disable=expression-not-assigned
 
-from tdsaf.main import Builder, TLS, DNS, UDP, ARP, EAPOL, ICMP, TCP, HTTP
+from tdsaf.main import Builder, TLS, DNS, UDP, ARP, EAPOL, ICMP, TCP, HTTP, MQTT
 from tdsaf.common.android import (
     LOCATION, BLUETOOTH, ADMINISTRATIVE, UNCATEGORIZED,
     NETWORK, RECORDING, STORAGE
@@ -38,12 +38,12 @@ udp_broadcast_4 = system.broadcast(UDP(port=30012)) # All zeros
 
 # Defining relevant backend services
 tuya_1 = system.backend("Tuya Smart 1").serve(TLS, HTTP).dns("a1.tuyaeu.com") #18.193.211.120
-tuya_2 = system.backend("Tuya Smart 2").serve(TLS(port=8883)).dns("m1.tuyaeu.com") #18.194.10.142
+tuya_2 = system.backend("Tuya Smart 2").serve(TLS(port=8883), TLS, MQTT).dns("m1.tuyaeu.com") #18.194.10.142
 tuya_3 = system.backend("Tuya Smart 3").serve(TLS).dns("a3.tuyaeu.com") # 3.121.131.36
-tuya_4 = system.backend("Tuya Smart 4").serve(TLS(port=8886)).dns("m2.tuyaeu.com") # 3.66.126.37
-tuya_images = system.backend("Tuya Images").serve().dns("images.tuyaeu.com").dns("djivuyxezwp4s.cloudfront.net") # 3.164.68.75
-aws = system.backend("AWS").serve(TLS).dns("euimagesd2h2yqnfpu4gl5.cdn5th.com") # 18.165.122.35, ...
-iot_dns = system.backend("IoT DNS").serve(TLS).dns("h3.iot-dns.com") # 76.223.21.194
+tuya_4 = system.backend("Tuya Smart 4").serve(TLS(port=8886)).dns("m2.tuyaeu.com") # 3.66.126.37 # Not found by shodan
+tuya_images = system.backend("Tuya Images").serve().dns("images.tuyaeu.com").dns("djivuyxezwp4s.cloudfront.net") # 3.164.68.75, not found by shodan
+aws = system.backend("AWS").serve(TLS).dns("euimagesd2h2yqnfpu4gl5.cdn5th.com") # 18.165.122.35, ... #not found by shodan
+iot_dns = system.backend("IoT DNS").serve(TLS).dns("h3.iot-dns.com") # 76.223.21.194 # not found by shodan
 tencent = system.backend("Tencent Cloud Computing").serve(TCP(port=443)).dns("tencent.com") # Did not respond # 162.14.14.21
 
 # Defining connections by the environment
